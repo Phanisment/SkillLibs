680b7ebaa24adde02db734425bdfc665dfe567ad
Phanisment <agilseptiyono94@gmail.com>
2024-10-20 13:26:35 +0700

    e

--- src/main/java/ph/test/Main.java
+++ src/main/java/ph/test/Main.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main extends ModInitializer {
+public class Main implements ModInitializer {
 	public static final String MOD_ID = "test";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	
