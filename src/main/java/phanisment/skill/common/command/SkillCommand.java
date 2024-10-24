fe44110b212d135173c06e45fb72b3f6c1cf50d1
Phanisment <agilseptiyono94@gmail.com>
2024-10-24 16:32:55 +0700

    e

--- src/main/java/phanisment/skill/common/command/SkillCommand.java
+++ src/main/java/phanisment/skill/common/command/SkillCommand.java
@@ -2,9 +2,9 @@ package phanisment.skill.common.command;
 
 import net.minecraft.entity.player.PlayerEntity;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.command.argument.TextArgumentType;
 import net.minecraft.text.Text;
 
 import static net.minecraft.server.command.CommandManager.literal;
@@ -15,16 +15,16 @@ import phanisment.skill.lib.Skill;
 public class SkillCommand {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		dispatcher.register(literal("castSkill")
-			.then(CommandManager.argument("skill", TextArgumentType.text())
+			.then(CommandManager.argument("skill", StringArgumentType.string())
 			.executes(context -> {
-				String skillName = TextArgumentType.getTextArgument(context, "skill");
+				String skillName = StringArgumentType.getString(context, "skill");
 				Skill skill = SkillRegistery.getSkill(skillName);
 				PlayerEntity player = context.getSource().getPlayer();
 				if (skill != null) {
 					skill.cast(player);
-					context.getSource().sendFeedback(Text.of("Casting " + skillName), false);
+					context.getSource().sendFeedback(Text.literal("Casting " + skillName), false);
 				} else {
-					context.getSource().sendError(Text.of("Skill not found!"));
+					context.getSource().sendError(Text.literal("Skill not found!"));
 				}
 				return 1;
 			}))
