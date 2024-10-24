b03e31af2e1b84bd521d2fb7ee31016523ca9962
Phanisment <agilseptiyono94@gmail.com>
2024-10-24 15:17:48 +0700

    e

--- src/main/java/phanisment/skill/common/command/SkillCommand.java
+++ src/main/java/phanisment/skill/common/command/SkillCommand.java
@@ -1,47 +1,30 @@
 package phanisment.skill.common.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.context.CommandContext;
-import net.minecraft.command.CommandSource;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
-import java.util.Collection;
+import static net.minecraft.server.command.CommandManager.literal;
 
-import phanisment.skill.util.SkillManager;
-import phanisment.skill.util.Skill;
+import phanisment.skill.util.SkillRegistery;
+import phanisment.skill.lib.Skill;
 
 public class SkillCommand {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-		dispatcher.register(
-			CommandManager.literal("castskill")
-				.then(CommandManager.argument("skill", StringArgumentType.string())
-					.suggests((context, builder) -> {
-						return CommandSource.suggestMatching(SkillManager.getAllSkillNames(), builder);
-					})
-					.then(CommandManager.argument("target", EntityArgumentType.players())
-						.executes(SkillCommand::castSkill))
-				)
+		dispatcher.register(literal("castSkill")
+			.then(CommandManager.argument("skillName", StringArgumentType.string())
+			.executes(context -> {
+				String skillName = StringArgumentType.getString(context, "skillName");
+				Skill skill = SkillManager.getSkill(skillName);
+				if (skill != null) {
+					skill.cast();
+					context.getSource().sendFeedback(Text.of("Casting " + skillName), false);
+				} else {
+					context.getSource().sendError(Text.of("Skill not found!"));
+				}
+				return 1;
+			}))
 		);
-	}
-
-	private static int castSkill(CommandContext<ServerCommandSource> context) {
-		String skillName = StringArgumentType.getString(context, "skill");
-		Collection<PlayerEntity> targets = EntityArgumentType.getPlayers(context, "target");
-		Skill skill = SkillManager.getSkill(skillName);
-		if (skill == null) {
-			context.getSource().sendError(Text.literal("Skill not found").formatted(Formatting.RED));
-			return 0;
-		}
-		for (PlayerEntity target : targets) {
-			target.sendMessage(Text.literal("Casting " + skillName + " on " + target.getName().getString()).formatted(Formatting.GREEN), false);
-			skill.cast(target);
-		}
-		return 1;
 	}
 }
